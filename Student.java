1fa308a84a069606b2898e60f14246ad280d86ad
trnkovskii <trnkovskiilija@yahoo.com>
2020-03-26 14:02:43 +0100

    student class finished”

--- Student.java
+++ Student.java
@@ -1,18 +1,67 @@
 class Student {
+
 	String index;
 	String firstName;
 	String lastName;
-	List<Integer> grades = new List<Integer>();
+	int[] grades = null;
 
-	//TODO constructor
-
-	//TODO seters & getters
-
-	public double getAverage() {
-		//TODO
+	public Student(String index, String firstName, String lastName, int[] grades) {
+		super();
+		this.index = index;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.grades = grades;
 	}
 
-	public int ECTSCredits() {
-		//TODO
+	public String getIndex() {
+		return index;
+	}
+
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int[] getGrades() {
+		return grades;
+	}
+
+	public void setGrades(int[] grades) {
+		this.grades = grades;
+	}
+
+	public double getAverage(int[] grades) {
+		int sum = 0;
+		for (int i = 0; i < grades.length; i++) {
+			sum += i;
+		}
+		return sum / grades.length;
+	}
+
+	public int ECTSCredits(int[] grades) {
+		int counter = 0;
+		for (int i = 0; i < grades.length; i++) {
+			if (grades[i] > 5) {
+				counter++;
+			}
+		}
+		return counter * 6;
 	}
 }
+
+
